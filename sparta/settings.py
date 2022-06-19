f823516b3cd712e46f1f737b3151f14d6c3153e6
jjy0307 <adz7546@gmail.com>
2022-06-19 14:59:27 +0900

    add new app

--- sparta/settings.py
+++ sparta/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS: List[str] = []
 # Application definition
 
 INSTALLED_APPS = [
+    "tabom.apps.TabomConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
